73179d72b5c78c8bdd4aa6b18edcda2b94232bc3
Abhee Hudani <62197910+Novartus@users.noreply.github.com>
2021-08-29 11:57:14 +0530

    Feature : Bean collection added in homepage, Logo added in Nabar and footer, Hamburger-menu logo added

---
+++ data/coffee.js
@@ -0,0 +1,34 @@
+const CoffeeCollectionData = {
+  data: [
+    {
+      imageURL: "/assets/data/coffee-bean-bag.png",
+      title: "Arabica",
+      description:
+        "Known as one of the most popular and well known types of coffee bean, Arabica beans are the most commonly produced variety and are considered higher quality beans. In fact, over 60% of the coffee beans in the world that are produced are Arabica variety.",
+      altText: "Arabica beans",
+    },
+    {
+      imageURL: "/assets/data/coffee-bean-bag.png",
+      title: "Robusta",
+      description:
+        "As the name hints, the Robusta bean is both hardy and is also immune to a variety of diseases too.",
+      altText: "Robusta beans",
+    },
+    {
+      imageURL: "/assets/data/coffee-bean-bag.png",
+      title: "Liberica",
+      description:
+        "One of the hardest types of coffee bean to come by is the Liberica bean. They are larger than the other beans and is the only one in the world that is known to have an irregular shape.",
+      altText: "Liberica beans",
+    },
+    {
+      imageURL: "/assets/data/coffee-bean-bag.png",
+      title: "Excelsa",
+      description:
+        "Although having recently been classified as a member of the Liberica family, the Excelsa bean is vastly different in taste when compared to the Liberica coffee bean.",
+      altText: "Excelsa beans",
+    },
+  ],
+};
+
+export default CoffeeCollectionData;
