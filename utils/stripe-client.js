9df6f6ae8b8016de00856aac5f66dbdf9e6b895b
Abhee Hudani <62197910+Novartus@users.noreply.github.com>
2021-09-04 02:12:18 +0530

    Fix : Missing Stripe Publishable key added

--- utils/stripe-client.js
+++ utils/stripe-client.js
@@ -5,7 +5,7 @@ let stripePromise;
 export const getStripe = async () => {
   if (!stripePromise) {
     stripePromise = await loadStripe(
-      `${process.env.PUBLIC_STRIPE_PUBLISHABLE_KEY}`
+      `pk_test_51It9p7K7gYarDdxebqWKRk8CPGeQqyUZDGpqyjcTJxS7acNWluTOauJkD9jCtqVUur1aSNv2wp2vOn8D02aWpaMg00N4XwXKkN`
     );
   }
 
