9edf2f4aab3800cd2855e6e6903b761e079d6846
Abhee Hudani <62197910+Novartus@users.noreply.github.com>
2021-09-04 00:15:55 +0530

    Feature : Payment Model Updated

--- Components/Payment.js
+++ Components/Payment.js
@@ -6,35 +6,28 @@ import {
   useStripe,
 } from "@stripe/react-stripe-js";
 import { getStripe } from "../utils/stripe-client";
+import styles from "../styles/Payment.module.scss";
 
 const CARD_OPTIONS = {
-  iconStyle: "solid",
   style: {
     base: {
-      iconColor: "#c4f0ff",
-      color: "#fff",
-      fontWeight: 500,
-      fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
-      fontSize: "16px",
+      color: "#32325d",
+      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
       fontSmoothing: "antialiased",
-      ":-webkit-autofill": {
-        color: "#fce883",
-      },
+      fontSize: "16px",
       "::placeholder": {
-        color: "#87BBFD",
+        color: "#aab7c4",
       },
     },
     invalid: {
-      iconColor: "#FFC7EE",
-      color: "#FFC7EE",
+      color: "#fa755a",
+      iconColor: "#fa755a",
     },
   },
 };
 
 const CardField = ({ onChange }) => (
-  <div className="FormRow">
-    <CardElement options={CARD_OPTIONS} onChange={onChange} />
-  </div>
+  <CardElement options={CARD_OPTIONS} onChange={onChange} />
 );
 
 const Field = ({
@@ -47,12 +40,9 @@ const Field = ({
   value,
   onChange,
 }) => (
-  <div className="FormRow">
-    <label htmlFor={id} className="FormRowLabel">
-      {label}
-    </label>
+  <div className={styles.modal_summary_details}>
+    <label htmlFor={id}>{label}</label>
     <input
-      className="FormRowInput"
       id={id}
       type={type}
       placeholder={placeholder}
@@ -88,17 +78,6 @@ const ErrorMessage = ({ children }) => (
     </svg>
     {children}
   </div>
-);
-
-const ResetButton = ({ onClick }) => (
-  <button type="button" className="ResetButton" onClick={onClick}>
-    <svg width="32px" height="32px" viewBox="0 0 32 32">
-      <path
-        fill="#FFF"
-        d="M15,7.05492878 C10.5000495,7.55237307 7,11.3674463 7,16 C7,20.9705627 11.0294373,25 16,25 C20.9705627,25 25,20.9705627 25,16 C25,15.3627484 24.4834055,14.8461538 23.8461538,14.8461538 C23.2089022,14.8461538 22.6923077,15.3627484 22.6923077,16 C22.6923077,19.6960595 19.6960595,22.6923077 16,22.6923077 C12.3039405,22.6923077 9.30769231,19.6960595 9.30769231,16 C9.30769231,12.3039405 12.3039405,9.30769231 16,9.30769231 L16,12.0841673 C16,12.1800431 16.0275652,12.2738974 16.0794108,12.354546 C16.2287368,12.5868311 16.5380938,12.6540826 16.7703788,12.5047565 L22.3457501,8.92058924 L22.3457501,8.92058924 C22.4060014,8.88185624 22.4572275,8.83063012 22.4959605,8.7703788 C22.6452866,8.53809377 22.5780351,8.22873685 22.3457501,8.07941076 L22.3457501,8.07941076 L16.7703788,4.49524351 C16.6897301,4.44339794 16.5958758,4.41583275 16.5,4.41583275 C16.2238576,4.41583275 16,4.63969037 16,4.91583275 L16,7 L15,7 L15,7.05492878 Z M16,32 C7.163444,32 0,24.836556 0,16 C0,7.163444 7.163444,0 16,0 C24.836556,0 32,7.163444 32,16 C32,24.836556 24.836556,32 16,32 Z"
-      />
-    </svg>
-  </button>
 );
 
 const CheckoutForm = () => {
@@ -147,104 +126,109 @@ const CheckoutForm = () => {
     }
   };
 
-  const reset = () => {
-    setError(null);
-    setProcessing(false);
-    setPaymentMethod(null);
-    setBillingDetails({
-      email: "",
-      phone: "",
-      name: "",
-    });
-  };
-
   return paymentMethod ? (
-    <div className="Result">
-      <div className="ResultTitle" role="alert">
-        Payment successful
+    <div className={styles.modal_summary_page_container}>
+      <div className={styles.modal_summary_container}>
+        <div className={styles.modal_summary_header}>
+          <h2>Payment successful</h2>
+        </div>
+        <p>
+          Thanks for trying Stripe Elements. No money was charged, but we
+          generated a PaymentMethod: {paymentMethod.id}
+        </p>
       </div>
-      <div className="ResultMessage">
-        Thanks for trying Stripe Elements. No money was charged, but we
-        generated a PaymentMethod: {paymentMethod.id}
-      </div>
-      <ResetButton onClick={reset} />
     </div>
   ) : (
-    <form className="Form" onSubmit={handleSubmit}>
-      <fieldset className="FormGroup">
-        <Field
-          label="Name"
-          id="name"
-          type="text"
-          placeholder="Jane Doe"
-          required
-          autoComplete="name"
-          value={billingDetails.name}
-          onChange={(e) => {
-            setBillingDetails({ ...billingDetails, name: e.target.value });
-          }}
-        />
-        <Field
-          label="Email"
-          id="email"
-          type="email"
-          placeholder="janedoe@gmail.com"
-          required
-          autoComplete="email"
-          value={billingDetails.email}
-          onChange={(e) => {
-            setBillingDetails({ ...billingDetails, email: e.target.value });
-          }}
-        />
-        <Field
-          label="Phone"
-          id="phone"
-          type="tel"
-          placeholder="(941) 555-0123"
-          required
-          autoComplete="tel"
-          value={billingDetails.phone}
-          onChange={(e) => {
-            setBillingDetails({ ...billingDetails, phone: e.target.value });
-          }}
-        />
-      </fieldset>
-      <fieldset className="FormGroup">
-        <CardField
-          onChange={(e) => {
-            setError(e.error);
-            setCardComplete(e.complete);
-          }}
-        />
-      </fieldset>
-      {error && <ErrorMessage>{error.message}</ErrorMessage>}
-      <SubmitButton processing={processing} error={error} disabled={!stripe}>
-        Pay $49.99
-      </SubmitButton>
-    </form>
+    <div className={styles.modal_summary_page_container}>
+      <div className={styles.modal_summary_container}>
+        <div className={styles.modal_summary_header}>
+          <h2>Checkout Summary</h2>
+        </div>
+        <div className={styles.modal_summary_content}>
+          <form onSubmit={handleSubmit}>
+            <Field
+              label="Name"
+              id="name"
+              type="text"
+              placeholder="Jane Doe"
+              required
+              autoComplete="name"
+              value={billingDetails.name}
+              onChange={(e) => {
+                setBillingDetails({
+                  ...billingDetails,
+                  name: e.target.value,
+                });
+              }}
+            />
+            <Field
+              label="Email"
+              id="email"
+              type="email"
+              placeholder="janedoe@gmail.com"
+              required
+              autoComplete="email"
+              value={billingDetails.email}
+              onChange={(e) => {
+                setBillingDetails({
+                  ...billingDetails,
+                  email: e.target.value,
+                });
+              }}
+            />
+            <Field
+              label="Phone"
+              id="phone"
+              type="tel"
+              placeholder="(941) 555-0123"
+              required
+              autoComplete="tel"
+              value={billingDetails.phone}
+              onChange={(e) => {
+                setBillingDetails({
+                  ...billingDetails,
+                  phone: e.target.value,
+                });
+              }}
+            />
+            <div className={styles.modal_summary_details}>
+              <CardField
+                onChange={(e) => {
+                  setError(e.error);
+                  setCardComplete(e.complete);
+                }}
+              />
+            </div>
+            {error && <ErrorMessage>{error.message}</ErrorMessage>}
+            <p>$49.99</p>
+            <div className={styles.modal_summary_checkout}>
+              <SubmitButton
+                processing={processing}
+                error={error}
+                disabled={!stripe}
+              >
+                Checkout
+              </SubmitButton>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
-};
-
-const ELEMENTS_OPTIONS = {
-  fonts: [
-    {
-      cssSrc: "https://fonts.googleapis.com/css?family=Roboto",
-    },
-  ],
 };
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
 const stripePromise = getStripe();
 
-const App = () => {
+const Payment = () => {
   return (
-    <div className="AppWrapper">
-      <Elements stripe={stripePromise} options={ELEMENTS_OPTIONS}>
+    <div>
+      <Elements stripe={stripePromise}>
         <CheckoutForm />
       </Elements>
     </div>
   );
 };
 
-export default App;
+export default Payment;
--- utils/stripe-client.js
+++ utils/stripe-client.js
@@ -3,9 +3,10 @@ import { loadStripe } from "@stripe/stripe-js";
 let stripePromise;
 
 export const getStripe = async () => {
-  console.log("process env", process.env);
   if (!stripePromise) {
-    stripePromise = await loadStripe(process.env.PUBLIC_STRIPE_PUBLISHABLE_KEY);
+    stripePromise = await loadStripe(
+      `${process.env.PUBLIC_STRIPE_PUBLISHABLE_KEY}`
+    );
   }
 
   return stripePromise;
